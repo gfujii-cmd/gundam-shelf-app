6478b8647a07a90b73e457b78f85af2f0f36a48b
Guilherme Fujii <gui.fujii@outlook.com>
2022-04-15 14:22:57 -0300

    fix: router issues
    Router tags moved to fit all the page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+
 import './App.scss';
 import Header from './shared/Header/index.jsx';
 import GundamList from './pages/GundamList/index'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import GundamForm from './components/GundamForm'
 
 const App = (props) => {
 
     return (
-        <>
-            <Header></Header>
-            <div className="main">
-               <BrowserRouter>
-                   <Routes>
-                        <Route path="/" element={<Navigate to="/list"></Navigate>}/>
-                        <Route path="/list" element={<GundamList></GundamList>}></Route>
-                   </Routes>
-               </BrowserRouter>
-            </div>
-        </>
+        <React.Fragment>
+               <Router>
+                   <Header></Header>
+                    <div className="main">
+                        <Routes>
+                            <Route path="/" exact element={<Navigate to="/list" replace></Navigate>}/>
+                            <Route path="/list" element={<GundamList></GundamList>}></Route>
+                            <Route path="/add" element={<GundamForm></GundamForm>}></Route>
+                        </Routes>
+                    </div>
+               </Router>
+        </React.Fragment>
     )
 }
 
