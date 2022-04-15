0d493d039d6fc060a8eae481a6f24a93e864d3ee
Guilherme Fujii <gui.fujii@outlook.com>
2022-04-15 13:23:55 -0300

    feat: routing added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import './App.scss';
 import Header from './shared/Header/index.jsx';
 import GundamList from './pages/GundamList/index'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 const App = (props) => {
+
     return (
         <>
             <Header></Header>
             <div className="main">
-                <GundamList></GundamList>
+               <BrowserRouter>
+                   <Routes>
+                        <Route path="/" element={<Navigate to="/list"></Navigate>}/>
+                        <Route path="/list" element={<GundamList></GundamList>}></Route>
+                   </Routes>
+               </BrowserRouter>
             </div>
         </>
     )
