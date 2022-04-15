4b0b5f3838cee26d9e9b70ec27500c95ebed3bd5
Guilherme Fujii <gui.fujii@outlook.com>
2022-04-15 13:22:53 -0300

    fix: deprecated method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from './App.jsx';
 
-ReactDOM.render(
-    <App />,
+ReactDOM.createRoot(
     document.getElementById('root')
-)
+).render(<App />)
