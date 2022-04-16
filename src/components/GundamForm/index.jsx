34b774e6591a54bea5280ab6fd22d8aa36937c52
Guilherme Fujii <gui.fujii@outlook.com>
2022-04-16 09:59:59 -0300

    feat: add new gundam

--- src/components/GundamForm/index.jsx
+++ src/components/GundamForm/index.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+const BASE_URL = 'http://localhost:3000/v1/gundams';
 
 const GundamForm = () => {
 
+    const navigate = useNavigate();
+
     const [name, setName] = useState('');
-    const [year, setYear] = useState(0);
+    const [year, setYear] = useState('');
     const [story, setStory] = useState('');
 
+    const sendData = (e) => {
+        e.preventDefault();
+        const newGundam = {
+            name: name,
+            year: parseInt(year, 10),
+            story: story
+        }
+        axios.post(`${BASE_URL}`, newGundam).then(navigate("/list"))
+    }
+
     return (
-        <form>
+        <form onSubmit={e => sendData(e)}>
             <div>
                 <label htmlFor="name">Name: </label>
                 <input id="name" value={name} onChange={e => setName(e.target.value)}></input>
