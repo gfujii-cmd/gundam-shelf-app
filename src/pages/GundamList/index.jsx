c9315cbe25174663a11ed8bc8b138f9cda53b003
Guilherme Fujii <gui.fujii@outlook.com>
2022-04-16 09:39:56 -0300

    feat: get all gundams

--- src/pages/GundamList/index.jsx
+++ src/pages/GundamList/index.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './index.scss';
+import axios from 'axios';
+
 import GundamCard from '../../components/GundamCard/index'
+
+const BASE_URL = 'http://localhost:3000/v1/gundams'
 
 const GundamList = () => {
 
+    const [gundamList, setGundamList] = useState([]);
+
+    const getAll = () => {
+        axios.get(BASE_URL).then(res => {
+            const response = res.data.response;
+            setGundamList(response);
+        })
+    }
+
+    useEffect(() => {
+        getAll();
+    }, [])
+
     return (
         <div className="gundam-grid">
-            <GundamCard year="2020" imageUrl={'https://grandtoyscollection.com.br/wp-content/uploads/2021/12/image-2-22-300x300.jpg'} title="MG Barbatos" story="Lorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor ametLorem ipsum dolor amet"></GundamCard>
-            <GundamCard year="2012" imageUrl={'https://grandtoyscollection.com.br/wp-content/uploads/2021/12/image-2-22-300x300.jpg'} title="MG Barbatos" story="Lorem ipsum dolor amet"></GundamCard>
+            {gundamList.map(gundam => <GundamCard key={gundam.id} year={gundam.year} imageUrl={'https://grandtoyscollection.com.br/wp-content/uploads/2021/12/image-2-22-300x300.jpg'} title={gundam.name} story={gundam.story}></GundamCard>)}
         </div>
     );
 }
